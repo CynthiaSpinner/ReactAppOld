fdae22e6fb324214cfb32e71dbcaef21b127c69e
Cynthia Spinner <CynthiaSpinner@gmail.com>
2025-09-29 16:11:10 -0500

    updated all react, to functions instead of classes, updated hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,35 @@
-import React from 'react'; //didnt mention to do this
+import React, {useState} from 'react'; //didnt mention to do this
 import './App.css';
 import FilmList from './components/filmList';
 
-class App extends React.Component { //functions are just as powerful as classes now
-  constructor(props) { 
-    super(props);
-    this.state = {
-      list: ["ready", "set", "GO"],
-      text: ""
-    };
-    this.onSubmit = this.onSubmit.bind(this); // not necessary can because "old"
-  }
+function App(props) {
+  const[list, setList] = useState(["ready", "set", "GO"]);
+  const[text, setText] = useState("");
 
-  onSubmit = (e) => { // arrow function already handles the bind, with ES6+
+  const onSubmit = (e) => {
     e.preventDefault();
-    this.setState({
-      list: [...this.state.list, this.state.text]
-    });
-  }
+    setList([...list, text]);
+  };
 
-  render() {
-    return (
-      <div>
-        <h1>HelloWorld</h1>
-        <form onSubmit={this.onSubmit}> {/*can just add prop here*/}
-          <input 
-            value={this.state.text}
-            onChange={(e) => this.setState({ text: e.target.value })}
-          ></input>
-          <button type="submit">Add</button>
-        </form>
-        <ul>
-          {this.state.list.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul>
-        <FilmList/>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>HelloWorld</h1>
+      <form onSubmit={onSubmit}> {/*can just add prop here*/}
+        <input 
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        ></input>
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {list.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+      <FilmList/>
+    </div>
+  );
 }
+
 
 export default App;
--- src/components/filmList.jsx
+++ src/components/filmList.jsx
@@ -1,39 +1,48 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-class FilmList extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            list: []
-        };
-    }
+function FilmList(props){
+    const[list, setList] = useState([]);
 
-    getFilms = () => {
+    const getFilms = () => {
         fetch('https://studioghibliapi-d6fc8.web.app/films')
             .then(response => response.json())
             .then(result => {
-                this.setState({
-                    list: result
-                });
+                setList(result);
             })
             .catch(error => {
-                console.error('Error fetching films:', error);
+                console.error('Error fetching films:', error);             
             });
     }
 
-    componentDidMount() {
-        this.getFilms();
-    }
+    useEffect(() => {
+        getFilms();
+    }, []);
 
-    render() {
-        return (
-            <ul>
-                {this.state.list.map(film => (
-                    <li key={film.id}>{film.title}</li>
-                ))}
-            </ul>
-        );
-    }
+    return (
+        <ul>
+            {list.map(film => (
+                <li key={film.id}>{film.title}</li>
+            ))}
+        </ul>
+    );
 }
+
+
+
+
+//     componentDidMount() {
+//         this.getFilms();
+//     }
+
+//     render() {
+//         return (
+//             <ul>
+//                 {this.state.list.map(film => (
+//                     <li key={film.id}>{film.title}</li>
+//                 ))}
+//             </ul>
+//         );
+//     }
+// }
 
 export default FilmList;
